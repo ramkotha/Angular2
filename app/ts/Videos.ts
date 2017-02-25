364734ef17b8b2c7989c627a7798d8a7dce74a5d
ram kotha <nagasatya.iis@gmail.com>
2017-02-25 12:28:07 -0600

    Angular2

---
+++ app/ts/Videos.ts
@@ -0,0 +1,18 @@
+export class Video {
+
+id:string;
+title:string;
+videocode:string;
+desc:string;
+
+constructor(id:number,title:string,videocode:string,desc:string){
+
+this.id=id;
+this.title=title;
+this.videocode=videocode;
+this.desc=desc;
+
+
+}
+
+}
--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,29 @@
+import {config} from './config.servive';
 import {Component} from 'angular2/core';
+import {videos} from './Videos';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: './app/ts/app.component.ts',
+	directives: [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	MainHeading = config.MAIN_HEADING;
+	videos:Array<video>;
+
+
+	constructor(){
+
+		this.Videos =[
+			new Video(1,"Learn Angular2","","How to study and learn Angular2"),
+			new Video(1,"Learn Angular2-1 ","","How to study and learn Angular2"),
+
+
+		]
+
+	}
+
+
+}
